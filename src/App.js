bf8d2130c0bf250cd6a220e1d6af0f5d5a34b13a
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2022-04-08 02:27:49 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
+import Data from './data.js';
 
 function App() {
+  let [data, data변경] = useState(Data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Codishop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="background">
+        <div className="background-content">
+          <h1>봄을 담은 옷, 최대 30% 얼리버드 쿠폰 </h1>
+          <p>4.1~4.11 역대급 웰컴 페스타</p>
+          <Button variant="primary" size="lg">
+            Primary button
+          </Button>{' '}
+        </div>
+      </div>
+     <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_960_720.jpg" width="100%" alt="" />
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://cdn.pixabay.com/photo/2020/04/22/06/47/hydrangea-5076212_960_720.jpg"
+              width="100%"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src="https://cdn.pixabay.com/photo/2021/07/26/14/31/woman-6494471_960_720.jpg" width="100%" alt="" />
+      <h4>{props.data[0].title}</h4>
+      <p>{props.data[0].content}</p>
+    </div>
+  );
+}
 export default App;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 0,
+    title: 'yellow dress',
+    content: 'Born in France',
+    price: 120000,
+  },
+
+  {
+    id: 1,
+    title: 'blue jacket',
+    content: 'Born in Seoul',
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: 'mauve-blouse',
+    content: 'Born in the States',
+    price: 130000,
+  },
+];
