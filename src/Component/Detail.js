87c6d7461d36f54c2fd93cb9a596a348c751c9dc
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2022-04-12 02:28:51 +0900

    .

--- src/Component/Detail.js
+++ src/Component/Detail.js
@@ -3,7 +3,7 @@ import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
 let 박스 = styled.div`
-  padding: 20px;
+  padding: 35px;
 `;
 let 제목 = styled.h4`
   fond-size=25px;
@@ -24,27 +24,18 @@ function Detail(props) {
       </박스>
 
       <div className="row">
-        <div className="col-md-6 mt-3">
+        <div className="col-md-6">
           <img src="/img/clothes1.jpg" width="100%" alt="" />
         </div>
-        <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{찾은상품.title}</h4>
-          <p>{찾은상품.content}</p>
-          <p>{찾은상품.price}원</p>
-          <button className="btn btn-danger">주문하기</button>
-          &nbsp;
-          <button
-            className="btn btn-danger"
-            onClick={() => {
-              history.goBack();
-            }}
-          >
-            뒤로가기
-          </button>
-          <div className="my-alert">
-            <p>재고가 얼마 남지 않았습니다.</p>
+        <div className="col-md-6 py-5">
+            <h4>{찾은상품.title}</h4>
+            <p>{찾은상품.content}</p>
+            <p>{찾은상품.price}원</p>
+            <button className="btn btn-danger mb-3">주문하기</button>
+            <div className="my-alert">
+              <p>재고가 얼마 남지 않았습니다.</p>
+            </div>
           </div>
-        </div>
       </div>
     </div>
   );
